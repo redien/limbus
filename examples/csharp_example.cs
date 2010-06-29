7e4571ab7928f3027d031057199da6add15f70e4
Jesper Oskarsson <jesosk@gmail.com>
2010-06-29 03:40:21 +0200

    Added C# binding generator. Fixed minor bugs in the class definitions.

--- examples/csharp_example.cs
+++ examples/csharp_example.cs
@@ -4,40 +4,49 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-using Placebo;
-using Opengl;
-
 public class OpenglWindowTest
 {
+	static private bool running;
+	
+	static private void CloseHandler( object sender )
+	{
+		running = false;
+	}
+	
+	static private void KeyDownHandler( object sender, int key, bool pressed )
+	{
+		if (pressed && key == Limbus.Imported.KeyEscape)
+			running = false;
+	}
+
 	static public void Main()
 	{
-		var window = new OpenglWindow();
-		window.caption = "C# Window";
-		window.width = 640;
-		window.height = 480;
-		window.create();
+		Limbus.OpenglWindow window = new Limbus.OpenglWindow();
+		window.Caption = "C# Window";
+		window.Width = 640;
+		window.Height = 480;
+		window.OnClose += new Limbus.OnCloseDelegate( CloseHandler );
+		window.Create();
+		
+		Limbus.Keyboard keyboard = new Limbus.Keyboard( window );
+		keyboard.OnKeyEvent += new Limbus.OnKeyEventDelegate( KeyDownHandler );
 
-		bool running = true;
-		window.onWindowEvent += e => { if (e == PlaceboC.WindowEventClose) running = false; };
-		window.onKeyEvent += ( key, pressed ) => { if (pressed && key == PlaceboC.KeyEscape) running = false; };
-
-		GL.glClearColor( 0.390625f, 0.58203125f, 0.92578125f, 1.0f );
-
-		var timer = new Timer();
+		running = true;
+		Limbus.Timer timer = new Limbus.Timer();
 		while (running)
 		{
-			window.sendEvents();
+			window.PollEvents();
+			keyboard.PollEvents();
+			window.SwapBuffers();
 
-			GL.glClear( GL.GL_COLOR_BUFFER_BIT );
-			window.swapBuffers();
-
-			if (timer.elapsed() > 1.0f)
+			if (timer.Elapsed > 1.0f)
 			{
 				System.Console.WriteLine( "Second passed..." );
-				timer.reset();
+				timer.Reset();
 			}
 		}
 
-		window.destruct();
+		keyboard.Destruct();
+		window.Destruct();
 	}
 }
