c7181c034f9ef8dc3c13579f904944280dc4e43b
Jesper Oskarsson <jesosk@gmail.com>
2014-08-06 18:42:08 +0200

    Removed licensing for cbind and dedicated bind to the public domain with CC0

--- dependencies/cbind/cdef_determine_types.lua
+++ dependencies/cbind/cdef_determine_types.lua
@@ -1,7 +1,13 @@
 
---     Copyright (c) 2009-2010 Jesper Oskarsson
--- This software is distributed under the MIT license.
---         (See accompanying file LICENSE.txt)
+-- cbind - A library for generating bindings.
+-- Written in 2014 by Jesper Oskarsson jesosk@gmail.com
+--
+-- To the extent possible under law, the author(s) have dedicated all copyright
+-- and related and neighboring rights to this software to the public domain worldwide.
+-- This software is distributed without any warranty.
+--
+-- You should have received a copy of the CC0 Public Domain Dedication along with this software.
+-- If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 require "lparse.match"
 
