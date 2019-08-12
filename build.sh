eb362dd530ac259b97fdc612ee09df4e26d8a557
Jesper Oskarsson <jesosk@gmail.com>
2019-08-12 21:20:02 +0200

    Build a dynamic library

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@
 project_root=..
 mkdir -p build ; cd build
 
+if [ -z "$SDL_PATH" ]; then
+    SDL_PATH=/usr/local/Cellar/sdl2/*
+fi
+SDL_INCLUDE_PATH=$SDL_PATH/include/SDL2
+SDL_LIB_PATH=$SDL_PATH/lib
+
 flags=-Wall
 
 cc -c $flags -I $project_root/include -I $SDL_INCLUDE_PATH $project_root/source/generic/*.c
@@ -10,4 +16,4 @@ cc -c $flags -I $project_root/include -I $SDL_INCLUDE_PATH $project_root/source/
 cc -c $flags -I $project_root/include -I $SDL_INCLUDE_PATH $project_root/source/platform/unix/**/*.c
 cc -c $flags -I $project_root/include -I $SDL_INCLUDE_PATH $project_root/source/util/*.c
 
-ar rcs liblimbus.a *.o
+cc -shared -fpic -L $SDL_LIB_PATH -lSDL2 *.o -o liblimbus.dylib
--- examples/c/build.sh
+++ examples/c/build.sh
@@ -5,14 +5,15 @@ mkdir -p build/examples ; cd build/examples
 
 flags="-Wall"
 limbusflags="-I $project_root/include -L $project_root/build/ -llimbus"
-sdlflags="-L $SDL_LIB_PATH -lSDL2"
 openglflags="-framework OpenGL"
 
 compile_example() {
-    cc -o $1 $flags $limbusflags $sdlflags $openglflags $project_root/examples/c/$1.c
+    cc -o $1 $flags $limbusflags $openglflags $project_root/examples/c/$1.c
 }
 
 compile_example opengl_example
 compile_example opengl_window
 compile_example screen_example
 compile_example timing_example
+
+cp ../liblimbus.dylib .
